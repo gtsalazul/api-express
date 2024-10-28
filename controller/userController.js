e32cff585e82bcc3eff2603730f7c8de7e664704
Carlos Vieira <carlos.vieira@digitalcollegesul.local>
2024-10-28 09:06:59 -0300

    feat: inclusao de metodos do user

--- controller/userController.js
+++ controller/userController.js
@@ -7,6 +7,34 @@ const getAllUsers = (_req, res) => {
     res.json(users);
 }
 
+const getUserById = (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find((u) => u.id == userId);
+    if (!user) {
+        const error = new Error("Usuário não encontrado!");
+        error.statusCode = 404;
+    }
+    res.json(user);
+}
+
+const createUser = (req, res) => {
+    const { nome, email } = req.body;
+    if (!nome || !email) {
+        return res
+        .status(400)
+        .json({error: "Nome e e-mail são obrigatórios!"})
+    }
+    const newUser = {
+        id: users.length + 1,
+        nome,
+        email
+    }
+    users.push(newUser);
+    res.status(201).json(newUser);
+}
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    getUserById,
+    createUser
 }
